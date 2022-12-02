d05eb73f7e5410243b8a2fac6d428800c02284b5
Tayonna25 <botello2@msu.edu>
2022-12-02 15:01:35 -0500

    test

--- soundboard.js
+++ soundboard.js
@@ -3,3 +3,15 @@ dino.onclick = function() {
    var trexAudio = new Audio('t-rex-roar.mp3');
    trexAudio.play();
 }
+
+const dino2 = document.getElementById('man');
+dino2.onclick = function() {
+   var ManAudio = new Audio('man.wav');
+   ManAudio.play();
+}
+
+const dino3 = document.getElementById('app');
+dino3.onclick = function() {
+   var AppAudio = new Audio('applause.wav');
+   AppAudio.play();
+}
